70654f47c32e88b019c80854830d4851b8bbda7f
Molter <gabriel.molter@gmail.com>
2013-05-04 12:35:45 -0300

    merged

--- Android App/src/br/feevale/droidhospital/ListaLeitosActivity.java
+++ Android App/src/br/feevale/droidhospital/ListaLeitosActivity.java
@@ -10,85 +10,79 @@ import br.feevale.droidhospital.db.Leito;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
-public class ListaLeitosActivity extends Activity implements OnItemClickListener {
-	
-	public static final String ID_VALUE = "id";
-	
-	private ArrayList<Leito> leitos;
-	
-	protected void onCreate(Bundle savedInstanceState) {
+public class ListaLeitosActivity extends Activity implements
+		OnItemClickListener {
 
+	public static final String ID_VALUE = "id";
+
+	private ArrayList<Leito> leitos;
+
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.quartos_layout);
-		
+
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+				.permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+
 		ListView quartosListView = (ListView) findViewById(R.id.quartos_list_view);
-		
+
 		setUpLeitos();
-		
-		LeitosAdapter quartosAdapter= new LeitosAdapter( getApplicationContext(), leitos );
-		
-		quartosListView.setAdapter(quartosAdapter);
-		
+
+		LeitosAdapter leitosAdapter = new LeitosAdapter(
+				getApplicationContext(), leitos);
+
+		quartosListView.setAdapter(leitosAdapter);
+
 		quartosListView.setOnItemClickListener(this);
 	}
 
-
 	private void setUpLeitos() {
-		
-		//leitos = alimentaLeitos();
-		
-		ArrayList<Leito> newQuartos = new ArrayList<Leito>();
-		String ultimoQuarto = ""; 
-		
-		for( int i = 0; i < leitos.size(); i++ ) {
-			if( ultimoQuarto != leitos.get(i).getQuarto() ) {
-				newQuartos.add(leitos.get(i));
-				ultimoQuarto = leitos.get(i).getQuarto();
-			} else{
-				ultimoQuarto = leitos.get(i).getQuarto();
-			}
-		}
-		
-		leitos = newQuartos;
+		leitos = alimentaLeitos();
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private ArrayList<Leito> alimentaLeitos() {
-		
+
 		try {
 
 			Interpretador interpretador = new Interpretador();
-			
-			interpretador.setCdTransacao( Interpretador.LISTA_LEITOS );
-			
-			EnviaTransacao enviador = new EnviaTransacao( interpretador );
-			
+
+			interpretador.setCdTransacao(Interpretador.LISTA_LEITOS);
+
+			EnviaTransacao enviador = new EnviaTransacao(interpretador);
+
 			try {
-				
-	    		enviador.envia();
-	    		
-	    		return (ArrayList<Leito>) enviador.recebe();
-	    		
-	    	} finally {
-	    		enviador.fechaSocket();
-	    	}
-			
+
+				enviador.envia();
+
+				return (ArrayList<Leito>) enviador.recebe();
+
+			} finally {
+				enviador.fechaSocket();
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
-		//Toast.makeText(getApplicationContext(), "Quarto" + String.valueOf(id), Toast.LENGTH_LONG).show();
-		Intent intent = new Intent(getApplicationContext(), ListaPacientesActivity.class);
+	public void onItemClick(AdapterView<?> arg0, View view, int position,
+			long id) {
+		// Toast.makeText(getApplicationContext(), "Quarto" +
+		// String.valueOf(id), Toast.LENGTH_LONG).show();
+		Intent intent = new Intent(getApplicationContext(),
+				ListaPacientesActivity.class);
 		intent.putExtra(ID_VALUE, id);
 		startActivity(intent);
 	}
