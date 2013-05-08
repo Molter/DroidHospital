820c0fcd5f451d602afb0f814617c086243fa9a1
mstrey <mstrey@gmail.com>
2013-05-07 22:05:53 -0300

    ajustes na comunicação do ListaMedicamentos

--- Android App/src/br/feevale/droidhospital/ListaMedicamentosActivity.java
+++ Android App/src/br/feevale/droidhospital/ListaMedicamentosActivity.java
@@ -3,44 +3,89 @@ package br.feevale.droidhospital;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 import br.feevale.comunicacao.EnviaTransacao;
 import br.feevale.droidhospital.adapters.MedicamentosAdapter;
 import br.feevale.droidhospital.db.Interpretador;
-import br.feevale.droidhospital.db.Quarto;
+import br.feevale.droidhospital.db.MedicamentoDescription;
 import br.feevale.droidhospital.pojos.Medicamento;
 
 public class ListaMedicamentosActivity extends Activity implements OnItemClickListener {
 	public static final String ID_VALUE = "id";
 	ArrayList<Medicamento> medicamentos;
 	
+	ListView medicamentosListView;
+	EditText edit_busca;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.medicamentos);
 		
-		ListView medicamentosListView = (ListView) findViewById(R.id.lista_medicamentos);
+		medicamentosListView = (ListView) findViewById(R.id.lista_medicamentos);
 		
-		setUpMedicamentos();
+		setUpMedicamentos("");
 		
 		MedicamentosAdapter medicamentosAdapter= new MedicamentosAdapter(getApplicationContext(), medicamentos);
 		
 		medicamentosListView.setAdapter(medicamentosAdapter);
 		
 		medicamentosListView.setOnItemClickListener(this);
+		
+		edit_busca = (EditText) findViewById(R.id.edit_busca);
+		
+		edit_busca.addTextChangedListener(new TextWatcher() {
+
+	        public void afterTextChanged(Editable s) {
+	            // TODO Auto-generated method stub
+
+	        }
+
+	        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+	            // TODO Auto-generated method stub
+
+	        }
+
+	        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        		edit_busca.removeTextChangedListener(this);
+
+        		if (edit_busca.getText().toString().length() > 0){
+        			
+        			setUpMedicamentos(edit_busca.getText().toString());
+        			
+        		} 
+
+        		if (start == 50){
+     	    		Toast.makeText(getBaseContext(), "Limite do campo atingido." , Toast.LENGTH_SHORT).show();
+ 	     	    	edit_busca.setText(s.toString().substring(0, s.toString().length()-1));
+	     	    	edit_busca.setSelection(s.toString().length()-1);
+     	    	} 
+
+	        	atualizaLista();
+
+     	    	edit_busca.addTextChangedListener(this);
+	        } 
+
+	    });
+		
+		
 	}
 
 
-	private void setUpMedicamentos() {
+	@SuppressWarnings("unchecked")
+	private void setUpMedicamentos(String nome_medicamento) {
 		try {
 
-			Interpretador interpretador = new Interpretador();
+			MedicamentoDescription interpretador = new MedicamentoDescription(nome_medicamento);
 
 			interpretador.setCdTransacao(Interpretador.LISTA_MEDICAMENTOS);
 
@@ -50,7 +95,7 @@ public class ListaMedicamentosActivity extends Activity implements OnItemClickLi
 
 				enviador.envia();
 
-				medicamentos =  (ArrayList<Medicamento>) enviador.recebe();
+				medicamentos = (ArrayList<Medicamento>) enviador.recebe();
 
 			} finally {
 				enviador.fechaSocket();
@@ -68,8 +113,20 @@ public class ListaMedicamentosActivity extends Activity implements OnItemClickLi
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
 		//Toast.makeText(getApplicationContext(), "Medicamento" + String.valueOf(id), Toast.LENGTH_LONG).show();
-		Intent intent = new Intent(getApplicationContext(), ListaPacientesActivity.class);
+/*		Intent intent = new Intent(getApplicationContext(), ListaPacientesActivity.class);
 		intent.putExtra(ID_VALUE, id);
 		startActivity(intent);
+		// implementar retorno para tela de prescrição
+*/	
 	}
+	
+	private void atualizaLista(){
+
+		MedicamentosAdapter adapter = new MedicamentosAdapter(this, medicamentos);
+		medicamentosListView.setAdapter(adapter);
+
+		adapter.notifyDataSetChanged();
+		
+	}
+
 }
---
+++ Android App/src/br/feevale/droidhospital/db/MedicamentoDescription.java
@@ -0,0 +1,75 @@
+package br.feevale.droidhospital.db;
+
+
+public class MedicamentoDescription extends Interpretador {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 883548177569131505L;
+	private String idMedicamento;
+	private String principio;
+	private String laboratorio;
+	private String fantasia;
+	private String concentracao;
+	private String forma;
+
+	private String busca_medicamento;
+
+	public String getBusca_medicamento() {
+		return busca_medicamento;
+	}
+
+	public MedicamentoDescription(String busca_medicamento) {
+		this.busca_medicamento = busca_medicamento;
+	}
+
+	public String getIdMedicamento() {
+		return idMedicamento;
+	}
+
+	public void setIdMedicamento(String idMedicamento) {
+		this.idMedicamento = idMedicamento;
+	}
+
+	public String getPrincipio() {
+		return principio;
+	}
+
+	public void setPrincipio(String principio) {
+		this.principio = principio;
+	}
+
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public String getFantasia() {
+		return fantasia;
+	}
+
+	public void setFantasia(String fantasia) {
+		this.fantasia = fantasia;
+	}
+
+	public String getConcentracao() {
+		return concentracao;
+	}
+
+	public void setConcentracao(String concentracao) {
+		this.concentracao = concentracao;
+	}
+
+	public String getForma() {
+		return forma;
+	}
+
+	public void setForma(String forma) {
+		this.forma = forma;
+	}
+
+}
